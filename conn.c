baded4c20636300de837e44eec16b02b1aa99611
gilbert <gilbertgmtidn@gmail.com>
2023-05-26 06:14:42 +0000

    initial commit 1

---
+++ conn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+char nama[100];
+printf("Masukan nama anda : ");
+scanf("%s",nama);
+
+printf("Halo selamat datang %s selamat berbahagia\n",nama);
+
+return 0;
+
+
+}
