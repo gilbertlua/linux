ae8f68392402c1b9b838eb96728d38940293677c
gilbert lua <gilbertgmtidn@gmail.com>
2023-06-08 04:28:13 +0000

    my trip my adventure

--- conn.c
+++ conn.c
@@ -7,9 +7,7 @@ printf("Masukan nama anda : ");
 scanf("%s",nama);
 
 printf("Halo selamat datang %s selamat berbahagia\n",nama);
-
-  printf("%s adalah orang yang sangat setia dalam menjalankan pelayanan dalam kehidupan sehari-harinya",nama);
-  
+printf("%s adalah orang yang sangat setia dalam menjalankan pelayanan dalam kehidupan sehari-harinya\n",nama);
 return 0;
 
 
